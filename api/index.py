3ba8a175f4d3484deafda6a7c9c1a447850541d5
KK Huang <kaihg1028+github@gmail.com>
2023-12-11 15:56:38 +0800

    use flask framework2

---
+++ api/index.py
@@ -0,0 +1,30 @@
+from decouple import config
+from flask import Flask, request
+from telegram import Update, Bot
+from telegram.ext import CallbackContext
+
+# 初始化Flask應用和Telegram機器人
+app = Flask(__name__)
+TOKEN = config('TOKEN')
+bot = Bot(token=TOKEN)
+
+
+# 定義收到Telegram訊息時的處理函數
+def handle_message(update: Update, context: CallbackContext):
+    message_text = update.message.text
+    response_text = "From Bot: " + message_text
+    update.message.reply_text(response_text)
+
+
+# 設定Telegram機器人的webhook路徑，用於接收訊息
+@app.route(f'/{TOKEN}', methods=['POST'])
+def webhook():
+    json_str = request.get_json()
+    update = Update.de_json(json_str, bot)
+    handle_message(update, None)
+    return 'OK'
+
+
+if __name__ == '__main__':
+    # 啟動Flask應用
+    app.run(debug=True)
