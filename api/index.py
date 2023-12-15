5e1fee6dc20caede3e0d541b664c520e6dba01c1
KK Huang <kaihg1028+github@gmail.com>
2023-12-15 17:07:25 +0800

    Use OpenAPI to translate

--- api/index.py
+++ api/index.py
@@ -1,12 +1,15 @@
 import os
 
 from flask import Flask, request
+from openai import OpenAI
 from telegram import Update, Bot
 
 # 初始化Flask應用和Telegram機器人
 app = Flask(__name__)
 TOKEN = os.getenv("TOKEN")
 bot = Bot(token=TOKEN)
+
+client = OpenAI()
 
 
 # 新增簡單的首頁路由
@@ -18,9 +21,17 @@ def index():
 # 定義收到Telegram訊息時的處理函數
 async def handle_message(update: Update):
     message_text = update.message.text
-    response_text = "From Bot: " + message_text
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system",
+             "content": "You are an experienced translator. When you want to translate other languages into Chinese, you will use concise and short text and retain the original meaning. When you get a message which is in Chinese, translate it into English. If it is not Chinese, it will be translated into Traditional Chinese."},
+            {"role": "user",
+             "content": message_text}
+        ]
+    )
     async with bot:
-        await bot.send_message(chat_id=update.effective_chat.id, text=response_text)
+        await bot.send_message(chat_id=update.effective_chat.id, text=completion.choices[0].message.content)
 
 
 # 設定Telegram機器人的webhook路徑，用於接收訊息
