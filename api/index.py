620cbe7f79232c3189f631f81def42dc97b1bffe
KK Huang <kaihg1028+github@gmail.com>
2023-12-15 13:46:59 +0800

    change back to bot example

--- api/index.py
+++ api/index.py
@@ -1,15 +1,17 @@
 import os
 
 from flask import Flask, request
-from telegram import Update
-from telegram.ext import CallbackContext, ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, Bot
+from telegram.ext import ContextTypes
 
 # 初始化Flask應用和Telegram機器人
 app = Flask(__name__)
 TOKEN = os.getenv("TOKEN")
-# bot = Bot(token=TOKEN)
-application = ApplicationBuilder().token(TOKEN).build()
-uq = application.update_queue
+bot = Bot(token=TOKEN)
+
+
+# application = ApplicationBuilder().token(TOKEN).build()
+# uq = application.update_queue
 
 
 # 新增簡單的首頁路由
@@ -22,17 +24,16 @@ def index():
 async def handle_message(update: Update):
     message_text = update.message.text
     response_text = "From Bot: " + message_text
-    # await update.message.reply_text(response_text)
+    await update.message.reply_text(response_text)
 
-    await application.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
-    return update.message.text
+    # await application.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
 
 
 # 設定Telegram機器人的webhook路徑，用於接收訊息
 @app.route('/telegram', methods=['POST'])
 async def webhook():
     json_str = request.get_json()
-    update = Update.de_json(json_str, application.bot)
+    update = Update.de_json(json_str, bot)
     await handle_message(update)
     return 'OK'
 
@@ -47,8 +48,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    # start_handler = CommandHandler('start', start)
+    # application.add_handler(start_handler)
+    # application.add_handler(MessageHandler(filters.TEXT))
 
     # 啟動Flask應用
     app.run(debug=True)
